f72857cadcee6ecef5a1a210392905a1ad650944
Taufan Nicola <taufanderazor@gmail.com>
2022-05-12 19:23:46 +0700

    feat: commit kedua

--- app/src/main/java/com/topanlabs/demogithub/MainActivity.kt
+++ app/src/main/java/com/topanlabs/demogithub/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //commit kedua
         setSupportActionBar(binding.toolbar)
         //bikin home
         val navController = findNavController(R.id.nav_host_fragment_content_main)
